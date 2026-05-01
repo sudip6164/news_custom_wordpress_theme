974da803b7dc3e1c0b0794ebd5a3bab5b3b3f6c8
sudip6164 <sudippradhanadgj@gmail.com>
2026-05-01 16:25:00 +0545

    feat: footer template

--- wp-content/themes/news/footer.php
+++ wp-content/themes/news/footer.php
@@ -1,6 +1,113 @@
 	<footer id="footer">
 		<div class="container">
-			<p><?php echo esc_html( get_bloginfo( 'name' ) ); ?> &copy; <?php echo esc_html( gmdate( 'Y' ) ); ?></p>
+
+			<div class="footer-widgets-wrap row col-mb-50">
+
+				<div class="col-lg-2 col-sm-6">
+					<div class="widget widget_links">
+						<h4 class="mb-3 mb-sm-4 text-transform-none ls-0">Management</h4>
+						<ul>
+							<li><a href="#">About Us</a></li>
+							<li><a href="#">Careers</a></li>
+							<li><a href="#">Customers</a></li>
+							<li><a href="#">Forums</a></li>
+						</ul>
+					</div>
+				</div>
+
+				<div class="col-lg-2 col-sm-6">
+					<div class="widget widget_links">
+						<h4 class="mb-3 mb-sm-4 text-transform-none ls-0">Categories</h4>
+						<ul>
+							<li><a href="#">Real Estate</a></li>
+							<li><a href="#">Movers</a></li>
+							<li><a href="#">Stores</a></li>
+							<li><a href="#">Landing</a></li>
+						</ul>
+					</div>
+				</div>
+
+				<div class="col-lg-2 col-sm-6">
+					<div class="widget widget_links">
+						<h4 class="mb-3 mb-sm-4 text-transform-none ls-0">Support</h4>
+						<ul>
+							<li><a href="#">Help Center</a></li>
+							<li><a href="#">Email Us</a></li>
+						</ul>
+						<div class="mt-3">
+							<a href="#" class="social-icon si-small bg-dark h-bg-facebook" title="Facebook" target="_blank" rel="noopener">
+								<i class="fa-brands fa-facebook-f"></i>
+								<i class="fa-brands fa-facebook-f"></i>
+							</a>
+
+							<a href="#" class="social-icon si-small bg-dark h-bg-instagram" title="instagram" target="_blank" rel="noopener">
+								<i class="bi-instagram"></i>
+								<i class="bi-instagram"></i>
+							</a>
+
+							<a href="#" class="social-icon si-small bg-dark h-bg-x-twitter" title="twitter" target="_blank" rel="noopener">
+								<i class="fa-brands fa-x-twitter"></i>
+								<i class="fa-brands fa-x-twitter"></i>
+							</a>
+
+							<a href="#" class="social-icon si-small bg-dark h-bg-wikipedia" title="Apple Pay">
+								<i class="fa-brands fa-apple-pay"></i>
+								<i class="fa-brands fa-apple-pay"></i>
+							</a>
+						</div>
+					</div>
+				</div>
+
+				<div class="col-lg-3 col-sm-6">
+					<div class="widget widget_links">
+						<h4 class="mb-3 mb-sm-4 text-transform-none ls-0">Tag Cloud</h4>
+						<div class="tagcloud">
+							<a href="#">general</a>
+							<a href="#">videos</a>
+							<a href="#">music</a>
+							<a href="#">media</a>
+							<a href="#">photography</a>
+							<a href="#">parallax</a>
+							<a href="#">ecommerce</a>
+							<a href="#">terms</a>
+							<a href="#">coupons</a>
+							<a href="#">modern</a>
+							<a href="#">magazine</a>
+							<a href="#">bootstrap</a>
+							<a href="#">news</a>
+							<a href="#">blog</a>
+							<a href="#">wordpress</a>
+						</div>
+					</div>
+				</div>
+
+				<div class="col-lg-3 col-sm-6">
+					<div class="widget widget_links">
+						<h4 class="mb-3 mb-sm-4 text-transform-none ls-0">Download in Mobile</h4>
+						<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus beatae esse iure est, quam libero!</p>
+					</div>
+				</div>
+
+			</div>
+		</div>
+
+		<div id="copyrights">
+			<div class="container">
+
+				<div class="row align-items-center justify-content-between col-mb-30">
+					<div class="col-lg-auto text-center text-lg-start">
+						Copyrights &copy; <?php echo esc_html( gmdate( 'Y' ) ); ?> All Rights Reserved by <?php echo esc_html( get_bloginfo( 'name' ) ); ?>.<br>
+						<div class="copyright-links"><a href="#">Terms of Use</a> / <a href="#">Privacy Policy</a></div>
+					</div>
+
+					<div class="col-lg-auto text-center text-lg-start">
+						<div class="copyrights-menu copyright-links m-0">
+							<a href="#">Home</a>/<a href="#">About</a>/<a href="#">Features</a>/<a href="#">Portfolio</a>/<a href="#">FAQs</a>/<a href="#">Contact</a>
+						</div>
+					</div>
+				</div>
+
+			</div>
 		</div>
 	</footer>
 
