def9788ecd535a1ce1d9e6cc8f0d771c76ea480c
sudip6164 <sudippradhanadgj@gmail.com>
2026-05-01 14:32:10 +0545

    feat: add theme support for menus and nav

---
+++ wp-content/themes/news/inc/setup.php
@@ -0,0 +1,16 @@
+<?php
+
+function news_theme_setup() {
+	// Core theme features.
+	add_theme_support( 'title-tag' );
+	add_theme_support( 'post-thumbnails' );
+	add_theme_support( 'custom-logo' );
+	add_theme_support( 'menus' );
+
+	register_nav_menus(
+		array(
+			'primary_menu' => __( 'Primary Navigation'),
+		)
+	);
+}
+add_action( 'after_setup_theme', 'news_theme_setup' );
